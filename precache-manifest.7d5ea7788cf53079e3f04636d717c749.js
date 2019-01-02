0ab054661510d71674e3050b71118c6db770d720
Jaroslaw Trzybinski <jaroslaw.trzybinski@gmail.com>
2019-01-02 20:27:50 +0000

    Updates

---
+++ precache-manifest.7d5ea7788cf53079e3f04636d717c749.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4f7bed750c577ecd7c9",
+    "url": "/fend-p8-neighborhood-map-test/static/js/runtime~main.d4f7bed7.js"
+  },
+  {
+    "revision": "1ceb91ef82e7c369ff6a",
+    "url": "/fend-p8-neighborhood-map-test/static/js/main.1ceb91ef.chunk.js"
+  },
+  {
+    "revision": "c51ebb174cbadd35c6ba",
+    "url": "/fend-p8-neighborhood-map-test/static/js/1.c51ebb17.chunk.js"
+  },
+  {
+    "revision": "1ceb91ef82e7c369ff6a",
+    "url": "/fend-p8-neighborhood-map-test/static/css/main.08f1180e.chunk.css"
+  },
+  {
+    "revision": "8ef5770044ccccbb728da572dce56208",
+    "url": "/fend-p8-neighborhood-map-test/index.html"
+  }
+];
